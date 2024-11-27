99129f579259d48a487dad65cd6c2ae23b8439ed
Yekaditya Devadi <141273781+yekaditya15@users.noreply.github.com>
2024-11-27 19:46:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Student extends Person {}
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    set age(age) {
+        this._age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+}
+
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
